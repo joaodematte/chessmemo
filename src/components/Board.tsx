e51974cf5d0b71ffa3fc0543551f3d603f4d43e2
João Dematte <joaoggdematte@gmail.com>
2023-02-09 21:14:57 -0300

    feat: board border

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -14,7 +14,7 @@ export default function Board() {
   const parsedAlphabet = gameSettings.viewAs === 'black' ? BOARD.alphabet : [...BOARD.alphabet].reverse();
 
   return (
-    <div id="board" className="grid grid-cols-8 grid-rows-8">
+    <div id="board" className="grid grid-cols-8 grid-rows-8 border-4 border-slate-800 dark:border-slate-200">
       {parsedAlphabet.map((word, index) =>
         BOARD.numbers.map((number) => (
           <BoardSquare key={`${word}${number}`} word={word} number={number} even={index % 2 === 0} />
